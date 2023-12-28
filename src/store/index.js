9230d08651a5aeee028ab323fe614c47771400ac
nimnool <povertaylenko.e@gmail.com>
2023-12-28 12:10:18 +0200

    add store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,31 @@
-import { createStore } from 'vuex'
-import cartStore from "@/store/modules/chartStore.js";
-
-const debug = process.env.NODE_ENV !== 'production'
+import { createStore } from 'vuex';
+import axios from 'axios';
 
 export default createStore({
-  modules: {
-    cartStore,
+  state: {
+    statistics: []
+  },
+  mutations: {
+    SET_STATISTICS(state, statistics) {
+      state.statistics = statistics;
+    }
+  },
+  actions: {
+    async fetchStatistics({ commit }) {
+      try {
+        const response = await axios.get('http://localhost:1337/api/values');
+        if (response.data && Array.isArray(response.data.data)) {
+          const processedData = response.data.data.map(item => ({
+            id: item.id,
+            startHeight: '0',
+            height: `${item.attributes.value}%`,
+            text: item.attributes.text
+          }));
+          commit('SET_STATISTICS', processedData);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
   }
-})
+});
--- src/views/BusinessGrowSection/BusinessGrowSection.vue
+++ src/views/BusinessGrowSection/BusinessGrowSection.vue
@@ -1,41 +1,34 @@
+// local strapi + store
 <script setup>
-import { ref, onMounted, inject } from 'vue';
+import { computed, onMounted, inject, nextTick } from 'vue';
+import { useStore } from 'vuex';
 import gsap from 'gsap';
 import MainTitleText from "@/components/MainTitleText/MainTitleText.vue";
-import { fetchStatistic } from '@/store/statApi.js';
-import { nextTick } from "vue";
 
+const store = useStore();
 const isTouchDevice = inject('isTouchDevice');
-const values = ref([]);
+const values = computed(() => store.state.statistics);
 
 onMounted(async () => {
-  const response = await fetchStatistic();
-  if (response && response.data) {
-    values.value = response.data.map(item => ({
-      id: item.id,
-      startHeight: '0',
-      height: `${item.attributes.value}%`,
-      text: item.attributes.text
-    }));
+  await store.dispatch('fetchStatistics');
 
-    await nextTick();
+  await nextTick();
 
-    if (!isTouchDevice) {
-      gsap.utils.toArray('.chart-item').forEach((item, index) => {
-        gsap.fromTo(item,
-            { height: values.value[index].startHeight },
-            {
-              height: values.value[index].height,
-              duration: 0.5,
-              delay: 0.2,
-              scrollTrigger: {
-                trigger: '.chart-wrap',
-                start: "center bottom-=100",
-                toggleActions: "play none none none",
-              }
-            });
-      });
-    }
+  if (!isTouchDevice) {
+    gsap.utils.toArray('.chart-item').forEach((item, index) => {
+      gsap.fromTo(item,
+          { height: values.value[index].startHeight },
+          {
+            height: values.value[index].height,
+            duration: 0.5,
+            delay: 0.2,
+            scrollTrigger: {
+              trigger: '.chart-wrap',
+              start: "center bottom-=100",
+              toggleActions: "play none none none",
+            }
+          });
+    });
   }
 });
 </script>
@@ -62,3 +55,71 @@ onMounted(async () => {
 </template>
 
 <style src="./business-grow.scss" lang="scss"/>
+
+// local without API
+<!--<script setup>-->
+<!--import {ref, onMounted, inject} from 'vue';-->
+<!--import gsap from 'gsap';-->
+<!--import MainTitleText from "@/components/MainTitleText/MainTitleText.vue";-->
+
+<!--const isTouchDevice = inject('isTouchDevice');-->
+<!--const statData = ref([-->
+<!--  {id: 1, startHeight: '0', height: '27%', text: '27'},-->
+<!--  {id: 2, startHeight: '0', height: '63%', text: '63'},-->
+<!--  {id: 3, startHeight: '0', height: '61%', text: '61'},-->
+<!--  {id: 4, startHeight: '0', height: '58%', text: '58'},-->
+<!--  {id: 5, startHeight: '0', height: '31%', text: '31'},-->
+<!--  {id: 6, startHeight: '0', height: '48%', text: '48'},-->
+<!--  {id: 7, startHeight: '0', height: '63%', text: '63'},-->
+<!--  {id: 8, startHeight: '0', height: '55%', text: '55'},-->
+<!--  {id: 9, startHeight: '0', height: '63%', text: '63'},-->
+<!--  {id: 10, startHeight: '0', height: '71%', text: '71'},-->
+<!--  {id: 11, startHeight: '0', height: '76%', text: '76'},-->
+<!--  {id: 12, startHeight: '0', height: '88%', text: '88'},-->
+<!--]);-->
+
+
+
+<!--onMounted(() => {-->
+<!--  if (!isTouchDevice) {-->
+<!--    gsap.utils.toArray('.chart-item').forEach((item, index) => {-->
+<!--      gsap.fromTo(item,-->
+<!--          { height: statData.value[index].startHeight },-->
+<!--          {-->
+<!--            height: statData.value[index].height,-->
+<!--            duration: 0.5,-->
+<!--            delay: 0.2,-->
+<!--            scrollTrigger: {-->
+<!--              trigger: '.chart-wrap',-->
+<!--              start: "center bottom-=100",-->
+<!--              toggleActions: "play none none none",-->
+<!--            }-->
+<!--          });-->
+<!--    });-->
+<!--  }-->
+<!--});-->
+<!--</script>-->
+
+<!--<template>-->
+<!--  <section id="section3" class="section">-->
+<!--    <div class="business-grow">-->
+<!--      <main-title-text-->
+<!--          title="Consistent leads flow to streamline Your business growth."-->
+<!--          text="We combine disruptive marketing techniques with proven tech solutions to provide maximum business value."-->
+<!--      />-->
+<!--      <div class="chart-wrap">-->
+<!--        <div-->
+<!--            class="chart-item"-->
+<!--            v-for="stat in statData"-->
+<!--            :key="stat.id"-->
+<!--        >-->
+<!--          <span>{{stat.text}}</span>-->
+<!--          <div class="stat-value" :style="{ height: stat.height }"></div>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--  </section>-->
+<!--</template>-->
+
+<!--<style src="./business-grow.scss" lang="scss"/>-->
+
